9cb8352d10cee0e61cb5b769790237d03e9fde56
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-24 09:58:00 -0500

    extracting hip hop data

--- hiphop.js
+++ hiphop.js
@@ -5,7 +5,13 @@ const rp = require('request-promise');
 rp('https://lukes-projects.herokuapp.com/v1/hiphop')
     .then(function (data) {
         let albums = JSON.parse(data);
-        console.log(albums.result[0].title);
+
+        // for loop extracts album titles and puts them into array 'album'
+        let album = [];
+        let numAlbums = albums.result.length;
+        for(let i = 0; i < numAlbums; i++) {
+            album.push(albums.result[i].title); 
+        }
     })
     .catch(function (err) {
         console.log(err);
