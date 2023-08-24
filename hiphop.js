176750b4f5e05a19179fbf24d77c60d95a7c3de6
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-24 10:22:33 -0500

    file validation

--- hiphop.js
+++ hiphop.js
@@ -1,18 +1,62 @@
 // import modules
 const rp = require('request-promise');
+const fs = require('fs');
+const dataPath = 'favorite-albums.json';
+
+// check if data path exists
+let check = Boolean;
+if (fs.existsSync(dataPath)) {
+    check = true;
+} else {
+    check = false;
+}
+
+// if dataPath doesnt exist create it
+if (check === false) {
+    fs.openSync(dataPath, 'w');
+};
 
 // pull JSON data from app 
 rp('https://lukes-projects.herokuapp.com/v1/hiphop')
     .then(function (data) {
-        let albums = JSON.parse(data);
+        let albumsData = JSON.parse(data);
 
         // for loop extracts album titles and puts them into array 'album'
         let album = [];
-        let numAlbums = albums.result.length;
-        for(let i = 0; i < numAlbums; i++) {
-            album.push(albums.result[i].title); 
+        let numAlbums = albumsData.result.length;
+        for (let i = 0; i < numAlbums; i++) {
+            album.push(albumsData.result[i].title);
+
+
+
+
+            // check if dataPath exists
+            // try {
+            //     if (fs.existsSync(dataPath)) {
+            //         fs.appendFileSync(dataPath, album[i], err => {
+            //             if (err) console.log(err);
+            //         })
+            //     } else {
+            //         fs.writeFile('favorite-albums.json', album[i], err => {
+            //             if (err) console.log(err);
+            //         });
+            //     }
+            // } catch (err) {
+            //     console.log(err);
+            // }
+
         }
+
+        // writing albums to 'favorite-albums.json' file
+        // let dataPath = path.join(__dirname, 'favorite-albums.json');
+        // fs.writeFile('favorite-albums.json', err => {
+        //     if (err) console.log(err);
+        // });
+
+        // fs.appendFileSync(dataPath, item.data.title + '\n');
+
     })
     .catch(function (err) {
         console.log(err);
     });
+
