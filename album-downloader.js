ee896fd2f7f6562d882c384bfb9f9d0c5c50a6a3
Jeremy <the.jeremy.cobb@gmail.com>
2023-08-24 13:49:05 -0500

    write thumbnails of artist to html file

---
+++ album-downloader.js
@@ -0,0 +1,36 @@
+// import modules
+const rp = require('request-promise');
+const fs = require('fs');
+const path = require('path');
+
+
+// pull JSON data from app 
+rp('https://lukes-projects.herokuapp.com/v1/hiphop')
+    .then(function (data) {
+        let albumsData = JSON.parse(data);
+
+        // initalizing number of iterations
+        let numAlbums = albumsData.result.length;
+
+        // for loop drives code
+        for (let i = 0; i < numAlbums; i++) {
+            // create custom file path
+            let dataPath = path.format({
+                dir: 'downloads',
+                base: albumsData.result[i].id + '.html',
+            });
+            // save filenames
+            fs.openSync(dataPath, 'w');
+
+            let link = albumsData.result[i].thumbnail_image;
+
+            // save thumbnail img
+            let img = '<a href="' + link + '"><img style="border: 1px solid #ddd; border-radius: 4px; padding: 5px; width: 150px;" src="'+link+'"></img></a>';
+
+            // // write img to html file
+            fs.appendFileSync(dataPath, img);
+        }
+    })
+    .catch(function (err) {
+        console.log(err);
+    });
